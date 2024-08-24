09d0311d3e760110d81ba3ebda9119fee9765356
Samara Parcero <samaraparceroti@gmail.com>
2024-08-24 11:38:39 -0300

    melhorias

--- ExBiblioteca/biblioteca/core/views.py
+++ ExBiblioteca/biblioteca/core/views.py
@@ -1,41 +1,55 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
 from rest_framework import status
 from .models import Livro
 from .serializers import LivroSerializer
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+  def __init__(self, data,  **kwargs):
+    content = JSONRenderer().render(data)
+    kwargs['content_type'] = 'application/json'
+    super(JSONResponse, self).__init__(content, **kwargs)
+
+
 @csrf_exempt
 def livro_list_create(request):
-  if request.method == 'GET':
-   livros = Livro.objects.all()
-   serializer = LivroSerializer(livros, many=True)
-   return Response(serializer.data)
-  if request.method == 'POST':
-   serializer = LivroSerializer(data=request.data)
-  if serializer.is_valid():
-   serializer.save()
-   return Response(serializer.data,
- status=status.HTTP_201_CREATED)
-   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == "GET":
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = LivroSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+      return JSONResponse(status = status.HTTP_406_NOT_ACCEPTABLE)  
 
 
 @csrf_exempt
 def livro_detail(request, pk):
- livro = Livro.objects.get(pk=pk)
- if request.method == 'GET':
-  serializer = LivroSerializer(livro)
-  return Response(serializer.data)
 
- if request.method == 'PUT':
-  serializer = LivroSerializer(livro, data=request.data)
-  if serializer.is_valid():
-   serializer.save()
-   return Response(serializer.data)
-  return Response(serializer.errors,
-status=status.HTTP_400_BAD_REQUEST)
-  
- if request.method == 'DELETE':
-  livro.delete()
-  return Response(status=status.HTTP_204_NO_CONTENT)
+    livro = Livro.objects.get(pk=pk)
+
+    if request.method == "GET":
+        serializer = LivroSerializer(livro)
+        return JSONResponse(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = LivroSerializer(livro, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        livro.delete()
+        return JSONResponse(status=status.HTTP_204_NO_CONTENT)
+      
+    else:
+      return JSONResponse(status = status.HTTP_406_NOT_ACCEPTABLE)
